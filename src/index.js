c9816398d0b565ca66b98ea9c3c83c2b4e67ea2e
HabibJR <smhabib.abir2030@gmail.com>
2022-08-04 00:10:01 +0200

    function component and class component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+// functional component
 
-var index = 0;
-
-
-setInterval(() => {
-  const element = (
-    <h1 className="heading" tabIndex={index}>
-      <span>hello world {new Date().toLocaleTimeString()} </span>
-      <img className="img" src=""></img>
+/*function Clock({ locale }) {
+  return (
+    <h1 className="heading">
+      <span>hello world {new Date().toLocaleTimeString(locale)} </span>
     </h1>
   );
+}*/
 
 
-  ReactDOM.render(element, document.getElementById("root"));
-}, 1000)
+// vanilla  Class component
+/*class Clock{
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
 
+  ClockElement(locale) {
+    return (
+      <h1 className="clock">
+        hell {this.name} {new Date().toLocaleTimeString(locale)}
+      </h1>
+    );
+  }
+}
+const clockElement = new Clock("habib", 1999);
+ReactDOM.render(clockElement.ClockElement("bn-BD"), document.getElementById("root")); */
 
+// react  Class component
+class Clock extends React.Component {
+  render() {
+    return <h1 className="clock">hell {new Date().toLocaleTimeString(this.props.locale)}</h1>;
+  }
+}
 
- 
+ReactDOM.render(<Clock locale="bn-BD" />, document.getElementById("root"));
