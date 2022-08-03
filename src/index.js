753cfad31cd17004cfaa638c225a780f9d338a5e
HabibJR <smhabib.abir2030@gmail.com>
2022-08-03 19:58:02 +0200

    jsx element and rendering

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>hell world</h1>
-  </React.StrictMode>
-);
+var index = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+setInterval(() => {
+  const element = (
+    <h1 className="heading" tabIndex={index}>
+      <span>hello world {new Date().toLocaleTimeString()} </span>
+      <img className="img" src=""></img>
+    </h1>
+  );
+
+
+  ReactDOM.render(element, document.getElementById("root"));
+}, 1000)
+
+
+
+ 
